efe7d3e66283fe4047e8a701da8efffc86d0d948
hyunbin1 <kimgusqls1@gmail.com>
2021-07-13 15:54:23 +0900

    21/07/13 tempt test code in MemberApp

---
+++ core/src/main/java/spring/core/MemberApp.java
@@ -0,0 +1,18 @@
+package spring.core;
+
+import spring.core.member.Grade;
+import spring.core.member.Member;
+import spring.core.member.MemberService;
+import spring.core.memberImplement.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member1 = new Member(1L, "hyun", Grade.VIP);
+        memberService.join(member1);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member1 = " + member1.getName());
+        System.out.println("find member1 = " + findMember.getName());
+    }
+}
