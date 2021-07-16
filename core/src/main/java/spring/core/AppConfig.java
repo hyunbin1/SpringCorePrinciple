7102a1b2d069508d0db2065fa1a837fce466ba04
hyunbin1 <kimgusqls1@gmail.com>
2021-07-16 12:11:14 +0900

    21/07/15 exchange java AppConfig to Spring Configuration, it called DI Container(feat. Spring Container)

--- core/src/main/java/spring/core/AppConfig.java
+++ core/src/main/java/spring/core/AppConfig.java
@@ -1,5 +1,7 @@
 package spring.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
 import spring.core.discount.RateDiscountPolicy;
@@ -11,23 +13,26 @@ import spring.core.memberImplement.MemoryMemberRepository;
 import spring.core.order.OrderService;
 import spring.core.orderServiceImplement.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 // 실제 동작에 필요한 구현 객체를 생성한다. - SOLID 중 DIP를 실현하기 위한 것.
     // 필요한 데이터 생성자 주입
     // return에는 구현체에 어떤 인터페이스를 연결해줄 것인지 파라미터로 설정해준다
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public MemberRepository memberRepository(){
         // 메모리 구현체 가져오기
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- core/src/main/java/spring/core/MemberApp.java
+++ core/src/main/java/spring/core/MemberApp.java
@@ -1,5 +1,7 @@
 package spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.member.Grade;
 import spring.core.member.Member;
 import spring.core.member.MemberService;
@@ -7,8 +9,13 @@ import spring.core.memberImplement.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService;
+        // annotaiton = @주석을 찾아준다
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 첫 인자: AppConfig에서 참조 변수명, 두번째 인자: 객체.class
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member1 = new Member(1L, "hyun", Grade.VIP);
         memberService.join(member1);
 
