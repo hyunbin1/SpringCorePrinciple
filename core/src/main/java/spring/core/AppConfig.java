e7c792e1305c58d489c29f17b8707cdf38b12133
hyunbin1 <kimgusqls1@gmail.com>
2021-07-23 19:31:35 +0900

    21/07/23 Project BeanDefinition test Complete, and notice about contept of java(forEach) in concept.txt

--- core/src/main/java/spring/core/AppConfig.java
+++ core/src/main/java/spring/core/AppConfig.java
@@ -18,15 +18,18 @@ public class AppConfig {
 // 실제 동작에 필요한 구현 객체를 생성한다. - SOLID 중 DIP를 실현하기 위한 것.
     // 필요한 데이터 생성자 주입
     // return에는 구현체에 어떤 인터페이스를 연결해줄 것인지 파라미터로 설정해준다
+
     @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository(){
         // 메모리 구현체 가져오기
         return new MemoryMemberRepository();
     }
+
     @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
@@ -34,7 +37,7 @@ public class AppConfig {
 
     @Bean
     public DiscountPolicy discountPolicy(){
-//        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
+//      return new FixDiscountPolicy();
     }
 }
--- core/src/test/java/spring/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/spring/core/beanfind/ApplicationContextInfoTest.java
@@ -2,12 +2,12 @@ package spring.core.beanfind;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.AppConfig;
 
 
 class ApplicationContextInfoTest {
-
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
@@ -19,4 +19,24 @@ class ApplicationContextInfoTest {
             System.out.println("name = " + beanDefinitionName + " object = " + bean);
         }
     }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    // 이렇게 하면 spring에서 돌아가는 우리에게 불필요한 Bean을 보지 않고 우리가 만든 빈 내용만 볼 수있다.
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            //ROLE_INFRASTRUCTURE: 스프링 내부에서 사용하는 빈
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+    // 위 테스트 케이스를 실행하면 우리가 정의했던 Bean을 5개를 볼 수 있다.
+    // appConfig, memberService, memberRepository, orderService, discountPolicy 각각 클래스 인스턴스가 생성이 됨을 확인할 수 있다.
+
 }
